9a62a326708bf8e55af535de4a0cc731e391df9e
frankmaruf <frankmaruf@hotmail.com>
2021-02-21 21:52:08 +0600

    depoly 9

--- HighschoolSimpleWebSite/settings.py
+++ HighschoolSimpleWebSite/settings.py
@@ -169,6 +169,6 @@ MEDIA_ROOT  = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
