0bb6705dbb8c3a442370c39ee4b339934cb680e2
frankmaruf <frankmaruf@hotmail.com>
2021-02-21 21:50:08 +0600

    depoly 8

--- HighschoolSimpleWebSite/settings.py
+++ HighschoolSimpleWebSite/settings.py
@@ -55,13 +55,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'HighschoolSimpleWebSite.urls'
@@ -146,13 +146,14 @@ USE_TZ = True
 
 
 # STATIC_ROOT = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles',)
+
 
 
 
 ##Working
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles',)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
