bafe25a348613ea4eb752d05b8d08a61037926d3
frankmaruf <frankmaruf@hotmail.com>
2021-02-21 22:03:27 +0600

    depoly 12

--- HighschoolSimpleWebSite/settings.py
+++ HighschoolSimpleWebSite/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 
 ##Working
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles',)
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles',)
 
 STATIC_URL = '/static/'
 
